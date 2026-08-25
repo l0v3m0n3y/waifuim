325076e7d7cd965b5fd8f7af76ee4e2f0a0965da
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:13:59 +0300

    Add HTTPMethod enum for request methods

--- src/waifuim.swift
+++ src/waifuim.swift
@@ -20,6 +20,13 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
 
 public class Waifuim {
     private let api = "https://api.waifu.im"
